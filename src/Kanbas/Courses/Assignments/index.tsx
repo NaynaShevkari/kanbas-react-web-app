f71c9a56d62dae5d2adfe0dbe0a83a4451cf16d5
Nayna <shevkarinayna@gmail.com>
2024-07-03 17:33:04 -0400

    AssignmentsUpdated

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -1,92 +1,87 @@
 import { useParams } from "react-router";
-import { assignments } from "../../Database";
-import { FaPlus, FaSearch } from 'react-icons/fa';
+import { useSelector, useDispatch } from "react-redux";
+import { BsGripVertical, BsPlus } from "react-icons/bs";
 import { BiSolidDownArrow } from "react-icons/bi";
-import { IoEllipsisVertical } from "react-icons/io5";
-import { BsGripVertical, BsPlus } from 'react-icons/bs';
 import { PiNotePencilLight } from "react-icons/pi";
-import LessonControlButtons from '../Modules/LessonControlButtons';
+import { IoEllipsisVerticalCircleSharp } from "react-icons/io5";
+import AssignmentControlButton from "./AssignmentControlButton";
+import AssignmentControl from "./AssignementControl";
+import { deleteAssignment } from "./reducer";
 
 export default function Assignments() {
   const { cid } = useParams();
-  const assignmentList = assignments.filter(assignment => assignment.course === cid);
+  const { assignments } = useSelector((state: any) => state.assignmentsReducer);
+
+  const assignmentCid = assignments.filter((assignment: any) => assignment.course === cid);
+  const dispatch = useDispatch();
+  console.log(assignments)
 
   return (
-    <div id="wd-assignments" className="container active border border-0">
-      <div className="d-flex align-items-center my-3">
-        <div className="input-group">
-          <div className="input-group-text bg-white"><FaSearch /></div>
-          <input type="text" className="form-control" placeholder="Search for Assignments" />
-        </div>
-        <div className="ms-auto d-flex">
-          <button className="btn btn-outline-secondary me-2 d-flex align-items-center">
-            <FaPlus className="me-2 text-black" /> Group
-          </button>
-          <button className="btn btn-danger d-flex align-items-center">
-            <FaPlus className="me-2" /> Assignment
-          </button>
-        </div>
-      </div>
-
+    <div id="assignments">
+      <AssignmentControl cid={cid!} />
+<br/>
+<br/>
       <div className="list-group mb-5 fs-5 border-gray">
         <div className="list-group-item p-3 ps-2 bg-light">
           <div className="d-flex align-items-center justify-content-between">
             <h4 className="mb-0">
-              <BsGripVertical className="me-2 fs-3" />
-              <BiSolidDownArrow className="me-2 fs-6" />
-              <b>ASSIGNMENTS</b>
-            </h4>
-            <div className="d-flex align-items-center">
+                <BsGripVertical className="fs-2 me-3 " />
+                <BiSolidDownArrow className="fs-4 me-3 " />
+                <b>ASSIGNMENTS</b>
+              </h4> 
+            <div className=" d-flex align-items-center">
               <div style={{
                 border: "1px solid #c7cdd158",
                 borderRadius: "25px",
                 padding: "5px 10px",
                 marginRight: "10px"
               }}>
-                <span className="btn btn-outline-secondary me-2 text-black">
-                  40% of Total
+                <span className="btn btn-outline-secondary text-black">
+                  <b>40% of Total</b>
                 </span>
               </div>
-              <BsPlus className="fs-4 me-2" />
-              <IoEllipsisVertical className="fs-4" />
+              <BsPlus className="me-2 fs-2 " />
+              <IoEllipsisVerticalCircleSharp className="fs-3" />
             </div>
           </div>
         </div>
-      </div>
+        {
+          assignmentCid && assignmentCid.map((item: any) => (
+            <li className="wd-assignment-list-item list-group-item p-3" style={{ borderLeft: "5px solid green" }}>
+              <div className="row align-items-center">
+                <div className="col-auto">
+                  <BsGripVertical className="fs-4" />
+                </div>
+                <div className="col-auto">
+                  <a className="wd-assignment-link link-underline"
+                    href={`#/Kanbas/Courses/${cid}/Assignments/${item._id}`}>
+                    <PiNotePencilLight className="text-success fs-4" />
+                  </a>
 
-      {assignmentList.map(item => (
-        <div className="list-group rounded-0" key={item._id}>
-          <div className="list-group-item p-3" style={{ borderLeft: "4px solid green" }}>
-            <div className="row align-items-center">
-              <div className="col-auto">
-                <BsGripVertical className="fs-3" />
-              </div>
-              <div className="col-auto">
-                <PiNotePencilLight className="text-success fs-3" />
-              </div>
-              <div className="col">             
-                <a className="wd-assignment-link text-dark link-underline link-underline-opacity-0"
+                </div>
+                <div className="col">
+                  <a className="wd-assignment-link link-underline link-underline-opacity-0 text-dark"
                     href={`#/Kanbas/Courses/${cid}/Assignments/${item._id}`}>
                     <h5><b>{item.title}</b></h5>
                   </a>
-                <p className="mb-0">
+                  <p className="mb-0">
                   <span className="text-danger">Multiple Modules</span> | 
                   <b> Not available until </b> {item.available} | 
                   <b> Due </b> {item.due} | 
                   {item.points} pts
                 </p>
+                </div>
+                <div className="col">
+                  <AssignmentControlButton assignmentId={item._id} deleteAssignment={(assignmentId) => {
+                      dispatch(deleteAssignment(assignmentId));
+                    }} />
+                </div>
               </div>
-              <div className="col ms-auto">
-                <LessonControlButtons />
-              </div>
-            </div>
-          </div>
-        </div>
-      ))}
-
-<br /><br /><br /><br /><br /><br />
-       <br />
-     <br />
+            </li>
+          ))
+        }
+      </div>
+      <br /><br /><br /><br /><br /><br /><br /><br />
     </div>
   );
 }
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,5 +1,6 @@
+import store from "./store";
+import { Provider } from "react-redux";
 import "./styles.css";
-import React from 'react';
 import Dashboard from './Dashboard';
 import KanbasNavigation from './Navigation';
 import { Routes, Route, Navigate } from 'react-router-dom';
@@ -31,30 +32,32 @@ export default function Kanbas() {
   };
 
   return (
-    <div id="wd-kanbas" className="h-100">
-      <div className="d-flex h-100">
-        <div className="d-none d-md-block bg-black">
-          <KanbasNavigation />
+    <Provider store={store}>
+      <div id="wd-kanbas" className="h-100">
+        <div className="d-flex h-100">
+          <div className="d-none d-md-block bg-black">
+            <KanbasNavigation />
           </div>
           <div className="flex-fill p-4">
-          <Routes>
-            <Route path="/" element={<Navigate to="Dashboard" />} />
-            <Route path="Account" element={<h1>Account</h1>} />
-            <Route path="Dashboard" element={<Dashboard
-              courses={courses}
-              course={course}
-              setCourse={setCourse}
-              addNewCourse={addNewCourse}
-              deleteCourse={deleteCourse}
-              updateCourse={updateCourse}/>
-} />
-            <Route path="Courses/:cid/*" element={<Courses courses = {courses} />} />
-            <Route path="Calendar" element={<h1>Calendar</h1>} />
-            <Route path="Inbox" element={<h1>Inbox</h1>} />
-          </Routes>
+            <Routes>
+              <Route path="/" element={<Navigate to="Dashboard" />} />
+              <Route path="Account" element={<h1>Account</h1>} />
+              <Route path="Dashboard" element={<Dashboard
+                courses={courses}
+                course={course}
+                setCourse={setCourse}
+                addNewCourse={addNewCourse}
+                deleteCourse={deleteCourse}
+                updateCourse={updateCourse} />
+              } />
+              <Route path="Courses/:cid/*" element={<Courses courses={courses} />} />
+              <Route path="Calendar" element={<h1>Calendar</h1>} />
+              <Route path="Inbox" element={<h1>Inbox</h1>} />
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
